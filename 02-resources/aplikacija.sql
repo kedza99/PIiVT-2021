9478a94f02388d47f92080407f4354d95967f9e9
Aleksa Kedzic <aleksa.kedzic.17@singimail.rs>
2021-10-26 20:46:13 +0200

    Updated sql dump and postman collection

--- 02-resources/aplikacija.sql
+++ 02-resources/aplikacija.sql
@@ -17,11 +17,13 @@ CREATE TABLE IF NOT EXISTS `administrator` (
   `is_active` tinyint(1) unsigned NOT NULL DEFAULT 1,
   PRIMARY KEY (`administrator_id`),
   UNIQUE KEY `uq_administrator_username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*!40000 ALTER TABLE `administrator` DISABLE KEYS */;
 INSERT INTO `administrator` (`administrator_id`, `username`, `password_hash`, `is_active`) VALUES
-	(1, 'Zoran', '$2b$11$OXpPTBkfYOnTH6ctKg.qzu9zGauTMrmiUIy4oopZkKyFSlZl1g5eu', 1);
+	(1, 'Zoran', '$2b$11$OXpPTBkfYOnTH6ctKg.qzu9zGauTMrmiUIy4oopZkKyFSlZl1g5eu', 1),
+	(4, 'Marijan', '$2b$11$pmJ0cgLky0/0exFz1l2mXO7thu4eCrCI4WhwEH5xeslNELc82jcXu', 1),
+	(5, 'Borijan', '$2b$11$s/2qVsxq/LMBUW5lfrbUQOfO3utEm9sPTcMCoRHzNXAYSzrY27JYq', 1);
 /*!40000 ALTER TABLE `administrator` ENABLE KEYS */;
 
 DROP TABLE IF EXISTS `animator`;
@@ -38,12 +40,9 @@ CREATE TABLE IF NOT EXISTS `animator` (
   UNIQUE KEY `uq_animator_nickname` (`nickname`),
   KEY `fk_animator_special_offer_id` (`special_offer_id`),
   CONSTRAINT `fk_animator_special_offer_id` FOREIGN KEY (`special_offer_id`) REFERENCES `special_offer` (`special_offer_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*!40000 ALTER TABLE `animator` DISABLE KEYS */;
-INSERT INTO `animator` (`animator_id`, `name`, `surname`, `nickname`, `description`, `price`, `image_path`, `special_offer_id`) VALUES
-	(4, 'Aleksa', 'Nikolic', 'Alex', 'dsafadsfafsa', 25, 'static/uploads/2021/09/0583427d-13d3-4711-a9fc-dd20bf595cc5-bjorn.jpg', 1),
-	(6, 'Vasilije', 'Redzic', 'Vasa', 'ssdfasdfrwerfsdadsfagrewa', 100, 'static/uploads/2021/09/867ac442-e8da-4223-85eb-46e7af08290c-end.jpg', 5);
 /*!40000 ALTER TABLE `animator` ENABLE KEYS */;
 
 DROP TABLE IF EXISTS `animator_date`;
@@ -57,12 +56,6 @@ CREATE TABLE IF NOT EXISTS `animator_date` (
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*!40000 ALTER TABLE `animator_date` DISABLE KEYS */;
-INSERT INTO `animator_date` (`animator_date_id`, `animator_id`, `reserved_date`) VALUES
-	(2, 4, '2021-09-10'),
-	(3, 4, '2021-09-11'),
-	(5, 4, '2021-09-12'),
-	(8, 4, '2021-09-13'),
-	(4, 4, '2021/09/11');
 /*!40000 ALTER TABLE `animator_date` ENABLE KEYS */;
 
 DROP TABLE IF EXISTS `reservation`;
@@ -80,11 +73,9 @@ CREATE TABLE IF NOT EXISTS `reservation` (
   UNIQUE KEY `uq_reservation_client_email` (`client_email`),
   UNIQUE KEY `uq_reservation_animator_date_id` (`animator_date_id`),
   CONSTRAINT `fk_reservation_animator_date_id` FOREIGN KEY (`animator_date_id`) REFERENCES `animator_date` (`animator_date_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*!40000 ALTER TABLE `reservation` DISABLE KEYS */;
-INSERT INTO `reservation` (`reservation_id`, `client_name`, `client_surname`, `client_email`, `client_phone_number`, `postal_address`, `animator_date_id`, `created_at`) VALUES
-	(1, 'Aleksa', 'Kedzic', 'aleksa@singimail.rs', '0633334444', 'Cegarska43', 5, '2021-09-10 19:53:37');
 /*!40000 ALTER TABLE `reservation` ENABLE KEYS */;
 
 DROP TABLE IF EXISTS `special_offer`;
@@ -96,13 +87,17 @@ CREATE TABLE IF NOT EXISTS `special_offer` (
   `image_path` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`special_offer_id`),
   UNIQUE KEY `uq_special_offer_name` (`name`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*!40000 ALTER TABLE `special_offer` DISABLE KEYS */;
 INSERT INTO `special_offer` (`special_offer_id`, `name`, `description`, `video_url`, `image_path`) VALUES
-	(1, 'clown', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', '...', '/static/specialOffers/clown.png'),
-	(2, 'vivo', 'fsdfsdfds', 'realm', 'zzzzz.jpg'),
-	(5, 'dry ice specialist', 'ggg', 'realm', 'static/uploads/2021/09/4c1f6ac2-00cc-4218-8d98-b50588c64a99-65030cc4-bdc6-44ac-b009-c3cf4ae97596.jpg');
+	(7, 'LED show', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', 'YF39NtnfdxY', 'static/uploads/2021/10/e0c122f4-a777-4958-a864-40a3141c10de-led-show-bg.jpg'),
+	(8, 'Magician', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.', 'ovaLaF6Rmhg', 'static/uploads/2021/10/3cccdd8d-a2a4-48ec-81f3-ee67fbf1b780-sajt-madjionicar-pozadina-nova.jpg'),
+	(9, 'Clowns and balloons', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.', '2XN5mhdWoM0', 'static/uploads/2021/10/66f683fb-63c9-4716-96a6-569ea44920c2-sajt-klovnovi-pozadina-1.jpg'),
+	(10, 'face painting', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.', '_feon_ie5I4', 'static/uploads/2021/10/373d94a4-e778-45cc-bbb8-4e22bacdf107-sajt-fp-pozadina.jpg'),
+	(11, 'dry ice', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.', 'oVmIAqwgIRo', 'static/uploads/2021/10/ce9a3085-7059-4cce-8b27-a68112bb2480-suvi-ledi800px.jpg'),
+	(12, 'fire show', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.', 'yyTSYqBIXRQ', 'static/uploads/2021/10/a84bcbf0-d5cd-494a-aa88-4628c85a519c-vatra-800px.jpg'),
+	(13, 'stilt walk', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.', 'J6fnEvpUS4k', 'static/uploads/2021/10/9cbdcd66-9afc-41ca-8721-694029781162-Dzin-specijalne-ponuda-1024x427-1.jpg');
 /*!40000 ALTER TABLE `special_offer` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
